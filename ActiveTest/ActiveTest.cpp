2f3e2fd177c680196acabeba5e923704792c69bd
PetroffLab <apetroff@clarku.edu>
2018-07-09 18:08:19 -0400

    New Push

--- ActiveTest/ActiveTest.cpp
+++ ActiveTest/ActiveTest.cpp
@@ -11,8 +11,31 @@ vector<double> update_data(tk::spline s, double dt, double x, double v, double p
 
 // Main Function
 int main(){
+  int size = 100;
   cout << "Start" << endl;
-  tk::spline s = surface(100, 1.0);
+  tk::spline s = surface(size, 1.0);
+
+  double dt = 0.01;
+  double tEnd = 1.0;
+  int tTot = (int)(tEnd/dt);
+
+  ofstream file;
+  file.open("Particle.dat");
+  file << 0.75 << "\t" << 0 << endl;
+  double x = 0.75;
+  double v = 0;
+  double prob = 0.8;
+  double D = 0;
+  vector<double> newDat(2);
+
+  for(int t=0; t<=tTot; t++){
+    cout << "At time " << t*dt << endl;
+    newDat = update_data(s, dt, x, v, prob, size, D);
+    x = newDat[0];
+    v = newDat[1];
+    file << x << "\t" << v << endl;
+  }
+  file.close();
 }
 
 // Defined Functions
@@ -47,11 +70,12 @@ tk::spline surface(int size, double dev){
 //  *Updates the poisiton data for a single particle
 vector<double> update_data(tk::spline s, double dt, double x, double v, double prob, int size, double D){
   bool vdir = v >= 0;
-  bool sdir = s(x+pow(10,-8))-s(x) <= 0;
+  bool sdir = s(x+pow(10,-4))-s(x) <= 0;
   default_random_engine generator;
   normal_distribution<double> distribution(0.5,0.5);
   double dw = sqrt(dt)*distribution(generator);
-  double newv = -(s(x+pow(10,-8))-s(x))/pow(10,-8) - v;
+  double newv = -((s(x+pow(10,-4))-s(x))/pow(10,-4)) - v;
+  cout << newv << endl;
   double dx = (newv*dt) + (sqrt(D)*dw);
   vector<double> soln(2);
 
@@ -64,8 +88,8 @@ vector<double> update_data(tk::spline s, double dt, double x, double v, double p
       soln[0] = x + dx;
       soln[1] = dx/dt;
     } else{
-      soln[0] = x + -(s(x+pow(10,-8))-s(x))/pow(10,-8);
-      soln[1] = ((-(s(x+pow(10,-8))-s(x))/pow(10,-8)*dt) + (sqrt(D)*dw))/dt;
+      soln[0] = x + -(s(x+pow(10,-4))-s(x))/pow(10,-4);
+      soln[1] = ((-(s(x+pow(10,-4))-s(x))/pow(10,-4)*dt) + (sqrt(D)*dw))/dt;
     }
   }
 
