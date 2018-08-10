b0b12736393ad4978c5f333897cd0d4494358c2b
PetroffLab <apetroff@clarku.edu>
2018-08-10 19:42:21 -0400

    Added Randomness to Bandits

--- Kindergarten/KinderArmedBanditSimpler.cpp
+++ Kindergarten/KinderArmedBanditSimpler.cpp
@@ -5,14 +5,15 @@ using namespace std;
 
 default_random_engine generator;
 
-int TrueValues[10] = {3,-1,4,2,-4,0,-3,-2,1,5};
 double TrueDeviation[10] = {1,1,1,1,1,1,1,1,1,1};
 
-double rewards(int);
+double rewards(int, vector<double>);
 int findMax(vector<double>);
+int epsilonChoice(vector<double>);
+vector<double> initValues();
 
-double rewards(int arm){
-  normal_distribution<double> distribution(TrueValues[arm],TrueDeviation[arm]);
+double rewards(int arm, vector<double> values){
+  normal_distribution<double> distribution(values[arm],TrueDeviation[arm]);
   return distribution(generator);
 }
 
@@ -26,26 +27,81 @@ int findMax(vector<double> values){
   return max;
 }
 
+int epsilonChoice(vector<double> values){
+  double prob = (double) rand()/(RAND_MAX);
+  double epsilon = 0.1;
+
+  if(prob<(1-epsilon)){
+    return findMax(values);
+  } else{
+    int explore = 0;
+    int step = rand() % (values.size() - 1);
+    for(int i=0; i<step; ){
+      if(explore != findMax(values)){
+        i++;
+      }
+      explore++;
+    }
+    return explore;
+  }
+}
+
+vector<double> initValues(){
+  vector<double> values = {0,0,0,0,0,0,0,0,0,0};
+  for(int i=0; i<10; i++){
+    values[i] = (2*((double)rand()/RAND_MAX)) - 1;
+  }
+  return values;
+}
+
 int main(){
   cout << "Start The Kindergarten Bandit" << endl;
 
-  int tEnd = 100;
-  double alpha = 0.5;
+  int tEnd = 10000;
+  double alpha = 0.1;
+  vector<double> TrueValues = initValues();
+
+  cout << "The Number of Time Steps is " << tEnd << endl;
+  cout << endl;
+
+  cout << "Start Optimistic Greedy Model" << endl;
 
   vector<double> EstValues = {10,10,10,10,10,10,10,10,10,10};
 
   for(int t=0; t<tEnd; t++){
     int action = findMax(EstValues);
-    double reward = rewards(action);
+    double reward = rewards(action, TrueValues);
     EstValues[action] = EstValues[action] + (alpha * (reward - EstValues[action]));
   }
 
+  cout << "According to this Model the best arm is " << findMax(EstValues)
+  << " vs " << findMax(TrueValues) << endl;
+
   for(int i=0; i<EstValues.size(); i++){
     cout << "Estimated Values of arm " << i << ": " <<
-    EstValues[i] << " vs " << TrueValues[i] << endl;
+    EstValues[i] << "\t vs \t" << TrueValues[i] << endl;
+  }
+  cout << endl;
+
+  cout << "Start Epsilon-Greedy Model" << endl;
+
+  EstValues = {0,0,0,0,0,0,0,0,0,0};
+
+  for(int t=0; t<tEnd; t++){
+    int action = epsilonChoice(EstValues);
+    double reward = rewards(action, TrueValues);
+    EstValues[action] = EstValues[action] + (alpha * (reward - EstValues[action]));
   }
 
-  cout << "Bandit Caught" << endl;
+  cout << "According to this Model the best arm is " << findMax(EstValues)
+  << " vs " << findMax(TrueValues) << endl;
 
+  for(int i=0; i<EstValues.size(); i++){
+    cout << "Estimated Values of arm " << i << ": " <<
+    EstValues[i] << "\t vs \t" << TrueValues[i] << endl;
+  }
+  cout << endl;
+
+  cout << "Bandit Caught" << endl;
   return 0;
 }
